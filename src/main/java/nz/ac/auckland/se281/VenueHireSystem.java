9fa235c48c866d3e24561e70353f179cea53cb42
Subeen Ban <sban919@aucklanduni.ac.nz>
2024-04-19 16:57:56 +1200

    Improved code style by adding comments in VenueHireSystem.

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -12,16 +12,20 @@ public class VenueHireSystem {
 
   public VenueHireSystem() {}
 
+  // printVenues is a method that prints venues
   public void printVenues() {
+    // Check if venueList empty
     if (venueList.isEmpty()) {
       MessageCli.NO_VENUES.printMessage();
     } else {
+      // Get appropriate grammar, quantity, and singular or plural
       int venueListSize = venueList.size();
       String isOrAre = getIsOrAre(venueListSize);
       String quantity = getQuantityString(venueListSize);
       String singularOrPlural = getSingularOrPlural(venueListSize);
       MessageCli.NUMBER_VENUES.printMessage(isOrAre, quantity, singularOrPlural);
 
+      // Iterate through venueList and print details
       for (Venue venue : venueList) {
         if (systemDate == null) {
           venue.printDetails("");
@@ -32,40 +36,49 @@ public class VenueHireSystem {
     }
   }
 
+  // createVenue is a method that creates a new venue
   public void createVenue(
       String venueName, String venueCode, String capacityInput, String hireFeeInput) {
     if (nameIsNotEmpty(venueName)
         && isPositiveNumber(capacityInput, "capacity")
         && isPositiveNumber(hireFeeInput, "hire fee")
         && noDuplicateCodeExists(venueCode)) {
+      // Create new venue and add to venueList
       Venue newVenue = new Venue(venueName, venueCode, capacityInput, hireFeeInput);
       venueList.add(newVenue);
       MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
     }
   }
 
+  // setSystemDate is a method that sets the system date
   public void setSystemDate(String dateInput) {
     systemDate = dateInput;
     MessageCli.DATE_SET.printMessage(dateInput);
   }
 
+  // printSystemDate is a method that prints the system date
   public void printSystemDate() {
+    // If systemDate does not exist print message
     if (systemDate == null) {
       MessageCli.CURRENT_DATE.printMessage("not set");
     } else {
+      // If systemDate exists print systemDate
       MessageCli.CURRENT_DATE.printMessage(systemDate);
     }
   }
 
+  // makeBooking is a method that makes a booking
   public void makeBooking(String[] options) {
     if (systemDate == null) {
       MessageCli.BOOKING_NOT_MADE_DATE_NOT_SET.printMessage();
     } else if (venueList.isEmpty()) {
       MessageCli.BOOKING_NOT_MADE_NO_VENUES.printMessage();
     } else {
+      // Generate booking reference
       String bookingReference = BookingReferenceGenerator.generateBookingReference();
       String venueName = null;
       int venueCapacity = 0;
+      // Find venue in venueList by code
       for (Venue venue : venueList) {
         if (venue.isSameCode(options[0])) {
           venueName = venue.getName();
@@ -73,10 +86,12 @@ public class VenueHireSystem {
           break;
         }
       }
+      // Validate booking options
       if (isNotNull(venueName, options[0])
           && dateIsNotInPast(options[1])
           && isNotBooked(venueName, options[1])) {
         int attendees = Integer.parseInt(options[3]);
+        // Adjust attendees when attendees are more than 100% or less than 25% of the venueCapacity
         if (attendees > venueCapacity) {
           attendees = venueCapacity;
           MessageCli.BOOKING_ATTENDEES_ADJUSTED.printMessage(
@@ -86,6 +101,7 @@ public class VenueHireSystem {
           MessageCli.BOOKING_ATTENDEES_ADJUSTED.printMessage(
               options[3], Integer.toString(attendees), Integer.toString(venueCapacity));
         }
+        // Create a new booking and add to bookingList
         Booking newBooking =
             new Booking(
                 venueName, bookingReference, options[1], options[2], Integer.toString(attendees));
@@ -96,9 +112,11 @@ public class VenueHireSystem {
     }
   }
 
+  // printBookings is a method that prints bookings for a specific venue
   public void printBookings(String venueCode) {
     if (venueExists(venueCode)) {
       if (!bookingExists(venueCode)) {
+        // Print message when existing venue has no bookings
         for (Venue venue : venueList) {
           if (venue.isSameCode(venueCode)) {
             String venueName = venue.getName();
@@ -107,14 +125,17 @@ public class VenueHireSystem {
         }
       }
     } else {
+      // Print message when venue does not exist
       MessageCli.PRINT_BOOKINGS_VENUE_NOT_FOUND.printMessage(venueCode);
     }
   }
 
+  // addCateringService is a method that adds catering service to a booking
   public void addCateringService(String bookingReference, CateringType cateringType) {
     if (bookingReferenceExists(bookingReference)) {
       for (Booking booking : bookingList) {
         if (booking.getBookingReference().equals(bookingReference)) {
+          // Create a new catering service and add to serviceList
           Catering newService =
               new Catering(bookingReference, booking.getAttendees(), cateringType);
           serviceList.add(newService);
@@ -123,28 +144,34 @@ public class VenueHireSystem {
       String service = "Catering (" + cateringType.getName() + ")";
       MessageCli.ADD_SERVICE_SUCCESSFUL.printMessage(service, bookingReference);
     } else {
+      // Print error message when booking does not exist
       MessageCli.SERVICE_NOT_ADDED_BOOKING_NOT_FOUND.printMessage("Catering", bookingReference);
     }
   }
 
+  // addServiceMusic is a method that adds music service to a booking
   public void addServiceMusic(String bookingReference) {
     if (bookingReferenceExists(bookingReference)) {
       for (Booking booking : bookingList) {
         if (booking.getBookingReference().equals(bookingReference)) {
+          // Create a new music service and add to serviceList
           Music newService = new Music(bookingReference, booking.getAttendees());
           serviceList.add(newService);
         }
       }
       MessageCli.ADD_SERVICE_SUCCESSFUL.printMessage("Music", bookingReference);
     } else {
+      // Print error message when booking does not exist
       MessageCli.SERVICE_NOT_ADDED_BOOKING_NOT_FOUND.printMessage("Music", bookingReference);
     }
   }
 
+  // addServiceFloral is a method that adds floral service to a booking
   public void addServiceFloral(String bookingReference, FloralType floralType) {
     if (bookingReferenceExists(bookingReference)) {
       for (Booking booking : bookingList) {
         if (booking.getBookingReference().equals(bookingReference)) {
+          // Create a new floral service and add to serviceList
           Floral newService = new Floral(bookingReference, booking.getAttendees(), floralType);
           serviceList.add(newService);
         }
@@ -152,14 +179,17 @@ public class VenueHireSystem {
       String service = "Floral (" + floralType.getName() + ")";
       MessageCli.ADD_SERVICE_SUCCESSFUL.printMessage(service, bookingReference);
     } else {
+      // Print error message when booking does not exist
       MessageCli.SERVICE_NOT_ADDED_BOOKING_NOT_FOUND.printMessage("Floral", bookingReference);
     }
   }
 
+  // viewInvoice is a method that views invoice for a booking
   public void viewInvoice(String bookingReference) {
     if (bookingReferenceExists(bookingReference)) {
       for (Booking booking : bookingList) {
         if (booking.getBookingReference().equals(bookingReference)) {
+          // Print bottom half of invoice
           MessageCli.INVOICE_CONTENT_TOP_HALF.printMessage(
               bookingReference,
               booking.getEmail(),
@@ -169,6 +199,7 @@ public class VenueHireSystem {
               booking.getVenueName());
 
           int totalCost = 0;
+          // Print venue hireFee and add to totalCost
           for (Venue venue : venueList) {
             if (venue.getName().equals(booking.getVenueName())) {
               totalCost += Integer.parseInt(venue.getHireFee());
@@ -176,6 +207,7 @@ public class VenueHireSystem {
             }
           }
 
+          // Print additional service costs and add to totalCost
           for (Service service : serviceList) {
             if (service.getBookingReference().equals(bookingReference)) {
               totalCost += service.cost();
@@ -195,14 +227,17 @@ public class VenueHireSystem {
               }
             }
           }
+          // Print bottom half of invoice with totalCost
           MessageCli.INVOICE_CONTENT_BOTTOM_HALF.printMessage(Integer.toString(totalCost));
         }
       }
     } else {
+      // Print error message when booking does not exist
       MessageCli.VIEW_INVOICE_BOOKING_NOT_FOUND.printMessage(bookingReference);
     }
   }
 
+  // isPositiveNumber is a method that checks if a string represents a positive number
   private boolean isPositiveNumber(String stringToCheck, String propartyName) {
     try {
       int parsedNumber = Integer.parseInt(stringToCheck);
@@ -217,6 +252,7 @@ public class VenueHireSystem {
     }
   }
 
+  // getIsOrAre is a method that gets the appropriate grammar 'is' or 'are'
   private String getIsOrAre(int venueListSize) {
     if (venueListSize == 1) {
       return "is";
@@ -225,6 +261,7 @@ public class VenueHireSystem {
     }
   }
 
+  // getQuantityString is a method that gets the appropriate quantity string for venue count
   private String getQuantityString(int venueListSize) {
     if (venueListSize < 10) {
       String[] numberNames = {
@@ -236,6 +273,7 @@ public class VenueHireSystem {
     }
   }
 
+  // getSingularOrPlural is a method that gets if venueList has a single venue or multiple venues
   private String getSingularOrPlural(int venueListSize) {
     if (venueListSize == 1) {
       return "";
@@ -244,6 +282,7 @@ public class VenueHireSystem {
     }
   }
 
+  // noDuplicateCodeExists is a method that checks is venueCode already exists in system
   private boolean noDuplicateCodeExists(String venueCode) {
     for (Venue venue : venueList) {
       if (venue.isSameCode(venueCode)) {
@@ -254,6 +293,7 @@ public class VenueHireSystem {
     return true;
   }
 
+  // nameIsNotEmpty is a method that checks if venue name is empty
   private boolean nameIsNotEmpty(String venueName) {
     if (venueName.trim().isEmpty()) {
       MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
@@ -262,6 +302,7 @@ public class VenueHireSystem {
     return true;
   }
 
+  // isNotBooked is a method that checks if a venue is not already booked on a specific date
   private boolean isNotBooked(String venueName, String bookingDate) {
     for (Booking booking : bookingList) {
       if (booking.isSameBooking(venueName, bookingDate)) {
@@ -272,7 +313,9 @@ public class VenueHireSystem {
     return true;
   }
 
+  // dateIsNotpast is a method that checks if booking date is not in the past
   private boolean dateIsNotInPast(String bookingDate) {
+    // Compare bookingDate with the systemDate
     String[] bookingDateParts = bookingDate.split("/");
     int bookingDay = Integer.parseInt(bookingDateParts[0]);
     int bookingMonth = Integer.parseInt(bookingDateParts[1]);
@@ -283,6 +326,7 @@ public class VenueHireSystem {
     int systemMonth = Integer.parseInt(systemDateParts[1]);
     int systemYear = Integer.parseInt(systemDateParts[2]);
 
+    // Compare in order of year to month to day
     if (bookingYear > systemYear) {
       return true;
     } else if (bookingYear == systemYear) {
@@ -298,6 +342,7 @@ public class VenueHireSystem {
     return false;
   }
 
+  // isNotNull is a method that finds if venue name is null and prints error message when it is
   private boolean isNotNull(String venueName, String venueCode) {
     if (venueName == null) {
       MessageCli.BOOKING_NOT_MADE_VENUE_NOT_FOUND.printMessage(venueCode);
@@ -306,6 +351,7 @@ public class VenueHireSystem {
     return true;
   }
 
+  // increaseDate is a method that increases a given date
   private String increaseDate(String dateToIncrease) {
     String[] dateToIncreaseParts = dateToIncrease.split("/");
     int dateToIncreaseDay = Integer.parseInt(dateToIncreaseParts[0]);
@@ -314,6 +360,7 @@ public class VenueHireSystem {
 
     int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30};
 
+    // Increase in order of day to month to year
     dateToIncreaseDay++;
     if (dateToIncreaseDay > daysInMonth[dateToIncreaseMonth - 1]) {
       dateToIncreaseDay = 1;
@@ -327,14 +374,17 @@ public class VenueHireSystem {
         "%02d/%02d/%04d", dateToIncreaseDay, dateToIncreaseMonth, dateToIncreaseYear);
   }
 
+  // nextAvailableDate is a method that finds the next available date for a venue
   private String nextAvailableDate(String venueName) {
     boolean dateAvailability = false;
     String nextAvailableDate = systemDate;
 
+    // If venue has no bookings return systemDate which is the fastest date
     if (bookingList == null) {
       return systemDate;
     }
 
+    // Check if date is available and when not increase date and check until available date
     while (!dateAvailability) {
       dateAvailability = true;
       for (Booking booking : bookingList) {
@@ -349,6 +399,7 @@ public class VenueHireSystem {
     return nextAvailableDate;
   }
 
+  // venueExists is a method that checks if venue exists
   private boolean venueExists(String venueCode) {
     for (Venue venue : venueList) {
       if (venue.isSameCode(venueCode)) {
@@ -359,6 +410,7 @@ public class VenueHireSystem {
     return false;
   }
 
+  // bookingExists is a method that checks if booking exists
   private boolean bookingExists(String venueCode) {
     boolean bookingExists = false;
     for (Booking booking : bookingList) {
@@ -379,6 +431,7 @@ public class VenueHireSystem {
     return false;
   }
 
+  // bookingReferenceExists is a method that checks if a booking exists
   private boolean bookingReferenceExists(String bookingReference) {
     for (Booking booking : bookingList) {
       if (booking.getBookingReference().equals(bookingReference)) {
